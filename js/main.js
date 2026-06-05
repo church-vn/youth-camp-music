998014593f7aad0ed0f15d7f72487df12552ac21
church-vn <wordoflife-vn@yandex.ru>
2026-06-05 12:59:08 +0300

    download_05_06_2025_3

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 400){
+        btn.style.display = 'block';
+    }else{
+        btn.style.display = 'none';
+    }
+
+});
+
+btn.addEventListener('click', () => {
+
+    document
+        .getElementById('back_to_top_id')
+        .scrollIntoView({
+            behavior: 'smooth'
+        });
+
+});
